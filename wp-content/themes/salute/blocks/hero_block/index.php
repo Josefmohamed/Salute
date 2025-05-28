f7471e5880d6918396858c46854a8deedf7cfa87
Youssef Mohamed <hendaoye.201250@gamil.com>
2025-05-28 10:26:17 +0300

    feat : hero_block not ready

--- wp-content/themes/salute/blocks/hero_block/index.php
+++ wp-content/themes/salute/blocks/hero_block/index.php
@@ -21,14 +21,46 @@ if (isset($block)) {
 }
 ?>
 <?php
-
+$image = get_field('image');
+$title = get_field('title');
+$description = get_field('description');
+$cta_button = get_field('cta_button');
 ?>
 <section id="<?= esc_attr($id) ?>" class="<?= esc_attr($className) ?>">
-  <div class="container">
-    <div class="content-wrapper">
-      <div class="left-content">left-content</div>
-      <div class="right-content">right-content</div>
+    <div class="images-animation-wrapper">
+        <?php
+        $picture_class = 'cover-image';
+        echo bis_get_attachment_picture(
+            $image,
+            [
+                375 => [375, 544, 1],
+                1024 => [1280, 630, 1],
+                1280 => [1280, 630, 1],
+                1440 => [1440, 1333, 1],
+            ],
+            [
+                'retina' => true, 'picture_class' => $picture_class,
+            ]
+        );
+        ?>
     </div>
+    <div class="container">
+        <div class="content column">
+            <?php if ($title): ?>
+                <h1 class="salute-h1 title white-color"><?= $title ?></h1>
+            <?php endif; ?>
+            <?php if ($description): ?>
+                <div class="paragraph-28 description white-color"><?= $description ?></div>
+            <?php endif; ?>
+                <?php if (!empty($cta_button) && is_array($cta_button)) { ?>
+                    <a class="cta-button light fw-400" href="<?= $cta_button['url'] ?>" target="<?= $cta_button['target'] ?>"><?= $cta_button['title'] ?>
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.678 15.9248H13.5872V6.21577C13.5872 4.50452 12.2582 3.11219 10.6247 3.11219H0V0.92334H10.6262C13.4119 0.92334 15.678 3.29731 15.678 6.21577V15.9248Z" fill="#011632"/>
+                            <path d="M1.44336 15.9247H0V13.7343H1.44336C5.91005 13.7343 9.54368 9.92754 9.54368 5.24805H11.6345C11.6345 8.09961 10.5742 10.7801 8.64975 12.7978C6.72528 14.8139 4.16525 15.9247 1.44336 15.9247Z" fill="#011632"/>
+                        </svg>
+                    </a>
+                <?php } ?>
+        </div>
   </div>
 </section>
 
