2a121bbf5f260a95982371ef28b91997d7187286
Youssef Mohamed <hendaoye.201250@gamil.com>
2025-07-27 19:53:15 +0300

    feat : header-logo update

--- wp-content/themes/salute/header.php
+++ wp-content/themes/salute/header.php
@@ -28,19 +28,16 @@ $header_logo = get_field('header_logo', 'options');
 <header class="salute-header <?= $header_version ?>">
   <div class="container">
       <div class="cards-wrapper">
-          <!--     logo-->
           <?php if ($header_logo) { ?>
               <a href="<?= site_url() ?>" target="_self" role="img" class="header-logo" aria-labelledby=" header_logo">
                   <?= \Theme\Helpers::display_attachment($header_logo, array("width" => 183, "height" => 46)) ?>
               </a>
           <?php } ?>
-          <!-- burger menu and cross-->
           <button aria-label="Open Menu Links" class="burger-menu">
               <span></span>
               <span></span>
               <span></span>
           </button>
-          <!--     links  -->
           <nav class="navbar">
               <div class="navbar-wrapper">
                   <?php if (have_rows('menu_links', 'options')) { ?>
